a16711d26ffb3c49a6ea6afde8bd5edc116de439
Sam-Cowman <sam.p.cowman@gmail.com>
2024-04-03 09:39:53 -0500

    Added comments for function

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,11 @@
 // const APIKey = '789080c772bf84186be0d95cea3d1c7b'
 
 function getWeather(cityName) {
-
+// Need to pull selected info based on city name 
+// Need to incorporate lag and log to get weather 
+// need humidity, temp and wind 
+// Potentially need a seperate function for 5 day forecast
+// Somehow need to save into local storage for rendering 
 }
 
 function renderSearchHistory() {
